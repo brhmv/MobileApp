61a30c9b021e94725a83a2c60591f6568fb4d8eb
brhmv <allahverdiibrahimov2004@gmail.com>
2024-09-03 16:18:47 +0400

    6. commit

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeStack from './stacks/HomeStack';
 import AuthStack from './stacks/AuthStack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
+
     return (
         <Tab.Navigator
             screenOptions={{
@@ -16,14 +19,26 @@ const TabNavigator = () => {
             <Tab.Screen
                 name="HomeStack"
                 component={HomeStack}
-                options={{ tabBarLabel: 'Home', headerShown: false }}
+                options={{
+                    tabBarLabel: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="home" size={size} color={color} />
+                    ),
+                }}
             />
             <Tab.Screen
                 name="AuthStack"
                 component={AuthStack}
-                options={{ tabBarLabel: 'Login', headerShown: false }}
-            />
+                options={{
+                    tabBarLabel: 'Login',
+                    headerShown: false,
 
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="person" size={size} color={color} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 };
